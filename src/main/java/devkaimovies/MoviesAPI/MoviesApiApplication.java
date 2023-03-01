f0128cb11fb37670a8a911517e0ece5393b70bb4
Kai Nagar <kaynagar@gmail.com>
2023-03-01 15:31:43 +0200

    Ready to work with all files good

---
+++ src/main/java/devkaimovies/MoviesAPI/MoviesApiApplication.java
@@ -0,0 +1,22 @@
+package devkaimovies.MoviesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@RestController
+public class MoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApiApplication.class, args);
+	}
+
+	//	#Simple Connect to localhost:8080 to check connection to server
+	//	@GetMapping("/")
+	//	public String apiRoot(){
+	//		return "Hello from Java!!!";
+	//	}
+
+}
---
+++ src/test/java/devkaimovies/MoviesAPI/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package devkaimovies.MoviesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
